b173c090ec9cb81995d3ebc873a9c63f41fa15a2
Vansh jhamb <vanshjhamb9@gmail.com>
2023-12-09 17:57:49 +0530

    Added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {FaArrowRight} from "react-icons/fa"
+import {Link} from "react-router-dom"
+import HighlightText from '../components/core/HomePage/HighlightText'
+
+import CTAButton from "../components/core/HomePage/Button"
+import Banner from "../assets/Images/banner.mp4"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
+import TimelineSection from '../components/core/HomePage/TimelineSection'
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from '../components/common/Footer'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
+
+const Home = () => {
+  return (
+    <div className='flex justify-center items-center text-3xl text-white'>
+      {/*Section1  */}
+      <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center 
+      text-white justify-between'>
+      Home Page
+
+</div>
+
+    </div>
+  )
+}
+
+export default Home
